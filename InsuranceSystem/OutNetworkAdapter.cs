cdc37a6c398f0da6cc32d65c0bdd4a2200848c87
JeffreyKayfes <jeffreykayfes@gmail.com>
2024-10-06 19:31:31 -0500

    Assignment Part 1 Completed

---
+++ InsuranceSystem/OutNetworkAdapter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing.Internal;
+
+namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    public class OutNetworkAdapter : InsuranceInterface
+    {
+        OutNetworkPatient patient;
+        public OutNetworkAdapter(string newName, int newPolicyNumber)
+        {
+            patient = new OutNetworkPatient(newName, newPolicyNumber);
+        }
+
+        public decimal CoverageAmount(int ProcedureID, decimal ProcedureCost)
+        {
+            decimal coveragePercent = patient.CoveragePercent(ProcedureCost);
+            return (coveragePercent * ProcedureCost);
+        }
+
+        public string getPatientName()
+        {
+            return patient.getPatientName();
+        }
+
+        public string getPolicyNumber()
+        {
+            return patient.PolicyNumber.ToString();
+        }
+
+        public bool IsCovered(string patientName, string policyNumber)
+        {
+            if (int.TryParse(policyNumber, out int policyNum))
+            {
+                string covered = patient.IsCovered(patientName, policyNum);
+                return covered == "yes";
+            }
+            return false;
+        }
+    }
+}
